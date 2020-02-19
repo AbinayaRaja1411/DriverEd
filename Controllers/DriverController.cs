10d1cbb17f9fb920d44c0cdd77a59baf7989881b
Abinaya Raja <abinayaraja1411@gmail.com>
2020-02-19 17:13:14 -0500

    Add project files.

---
+++ Controllers/DriverController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DriverEd.Controllers
+{
+    public class DriverController : Controller
+    {
+        public IActionResult ListDrivers()
+        {
+            return View();
+        }
+    }
+}
